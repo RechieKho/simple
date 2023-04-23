989b199118234173141e6df2d8c7a060148e14e0
RechieKho <the.real.richie.kho@gmail.com>
2023-04-24 07:45:47 +0800

    Fix `string.h` outdated identifiers.

--- types/string.h
+++ types/string.h
@@ -8,12 +8,12 @@
 /* A reference counted growable string. */
 typedef struct simple_types_string_t simple_types_string_t;
 
-simple_error_t simple_types_string_create(simple_types_string_t** m_string);
+simple_types_error_t simple_types_string_create(simple_types_string_t** m_string);
 void simple_types_string_retain(simple_types_string_t* m_string); // Increase reference count.
 void simple_types_string_release(simple_types_string_t* m_string); // Decrease reference count (and destroy if necessary).
 void simple_types_string_copy(simple_types_string_t** m_dest, const simple_types_string_t* p_source);
-simple_error_t simple_types_string_append(char p_character);
-simple_error_t simple_types_string_reserve(simple_types_string_t* m_string, size_t p_min_size); // Pre-allocate memory.
+simple_types_error_t simple_types_string_append(char p_character);
+simple_types_error_t simple_types_string_reserve(simple_types_string_t* m_string, size_t p_min_size); // Pre-allocate memory.
 size_t simple_types_string_size(const simple_types_string_t* p_string);
 const char* simple_types_string_data(const simple_types_string_t* p_string);
 
