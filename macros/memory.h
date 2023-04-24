316205cbeae63b7959c6156d5b388a160aeb923b
RechieKho <the.real.richie.kho@gmail.com>
2023-04-24 18:58:06 +0800

    Fix `memory.h` invalid identifier errors.

--- macros/memory.h
+++ macros/memory.h
@@ -5,17 +5,19 @@
 #include <stdbool.h>
 
 #include "types/error.h"
+#include "error.h"
 
-#define SIMPLE_MACROS_MEMORY_RESERVE(l_type, m_output, m_capacity, p_min_capacity, m_error) \
+#define SIMPLE_MACROS_MEMORY_INIT_CAPACITY 10
+
+#define SIMPLE_MACROS_MEMORY_RESERVE(l_type, m_output, p_capacity, p_min_capacity, m_error) \
     do { \
         (m_error) = SIMPLE_OK; \
-        size_t new_capacity = (m_capacity); \
+        size_t new_capacity = (p_capacity == 0)? SIMPLE_MACROS_MEMORY_INIT_CAPACITY : p_capacity; \
         while(new_capacity < (p_min_capacity)) new_capacity *= 2; \
         const size_t bytes = new_capacity * sizeof(l_type); \
-        if(((m_output) = (l_type*)realloc((vol*)(m_output), bytes)) == NULL) { \
-            (m_error) = SIMPLE_ALLOCATE_FAIL; \
-            (m_capacity) = 0; \
-            SIMPLE_PRINT_CANT_ALLOCATE_MACROS_MEMORY(bytes); \
+        if(((m_output) = (l_type*)realloc((void*)(m_output), bytes)) == NULL) { \
+            (m_error) = SIMPLE_FAIL_CANT_ALLOCATE_MEMORY; \
+            SIMPLE_MACROS_ERROR_PRINTF("Unable to reserve '%zu' bytes of memory.", bytes); \
         } \
     } while(false);
 
