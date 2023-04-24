0336b4d4b21a950f38511ea1f6cb94da15e4e9cd
RechieKho <the.real.richie.kho@gmail.com>
2023-04-24 10:18:36 +0800

    Fix early return syntax and argument error.

--- macros/error.h
+++ macros/error.h
@@ -5,10 +5,11 @@
 
 #define SIMPLE_MACROS_ERROR_HEADING_CSTRING "ERROR: "
 #define SIMPLE_MACROS_ERROR_INFO_CSTRING " [line %u, in file '%s']"
-#define SIMPLE_MACROS_ERROR_CSTRING(mp_message) SIMPLE_MACROS_ERROR_HEADING_CSTRING (mp_message) SIMPLE_MACROS_ERROR_INFO_CSTRING
+#define SIMPLE_MACROS_ERROR_CSTRING(p_message) \
+    (SIMPLE_MACROS_ERROR_HEADING_CSTRING p_message SIMPLE_MACROS_ERROR_INFO_CSTRING)
 
-#define SIMPLE_MACROS_ERROR_PRINT_CANT_ALLOCATE_MEMORY(mp_bytes) \
-    fprintf(stderr, SIMPLE_MACROS_ERROR_CSTRING("Unable to allocate %zu bytes of memory."), (mp_bytes), __LINE__, __FILE__)
+#define SIMPLE_MACROS_ERROR_PRINT_CANT_ALLOCATE_MEMORY(p_bytes) \
+    fprintf(stderr, SIMPLE_MACROS_ERROR_CSTRING("Unable to allocate %zu bytes of memory."), (p_bytes), __LINE__, __FILE__)
 
 #define SIMPLE_MACROS_ERROR_FAIL_COND_V_STEPS(p_condition, p_return_value, l_steps) \
     if (p_condition){ \
@@ -20,9 +21,8 @@
 
 #define SIMPLE_MACROS_ERROR_FAIL_COND_V_MSG(p_condition, p_return_value, p_message) \
     if (p_condition){ \
-        fputs(stderr, SIMPLE_MACROS_ERROR_CSTRING(p_message)); \
+        fputs(SIMPLE_MACROS_ERROR_CSTRING(p_message), stderr); \
         return (p_return_value); \
     }
-
 
 #endif//_SIMPLE_MACROS_ERROR_H_
